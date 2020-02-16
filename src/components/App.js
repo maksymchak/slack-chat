711854529b7d4336ccf3585ceb17818056e3c010
Anton Maksymchak <maksym4ak@gmail.com>
2020-02-16 17:04:50 +0200

    fix eslint

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return <div>App new</div>;
   }
 }
 
-const x;
-
 export default App;
+
+//New commits
